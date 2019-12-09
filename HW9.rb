f7be68090334b73c7ab66a07d542791e51b7036c
Sarah <alahmadisr@gmail.com>
2019-12-09 19:07:12 +0300

    one question remaining

---
+++ HW9.rb
@@ -0,0 +1,142 @@
+#1. Return an array of each Student's full name, upper-cased
+students = [
+  {
+      first_name: 'Abdulrahman',
+      last_name: 'Alsulami'
+  },
+  {
+      first_name: 'Leena',
+      last_name: 'Yaseen',
+  },
+  {
+      first_name: 'Sara',
+      last_name: 'Alraddadi',
+  }
+]
+
+upper_case_full_names = []
+for i in students
+    upper_case_full_names.push(i[:first_name].upcase+" "+i[:last_name].upcase)
+end
+p upper_case_full_names
+
+#-------------------------------------------------------------------------------------------------------
+
+#2. Find the first order for each user
+users = [
+  {
+      name: 'Bandar',
+      orders: [
+          {
+              description: 'a bike'
+          }
+      ]
+  },
+  {
+      name: 'Hatim',
+      orders: [
+          {
+              description: 'bees'
+          },
+          {
+              description: 'fishing rod'
+          }
+      ]
+  },
+  {
+      name: 'Mohammed',
+      orders: [
+          {
+              description: 'a MacBook'
+          },
+          {
+              description: 'The West Wing DVDs'
+          },
+          {
+              description: 'headphones'
+          },
+          {
+              description: 'a kitten'
+          }
+      ]
+  }
+]
+
+first_order_for_each_user = []
+for i in users
+    first_order_for_each_user.push(i[:orders][0])
+end
+p first_order_for_each_user
+
+#-------------------------------------------------------------------------------------------------------
+
+#3. Find the average amount spent on coffee, per transaction, for each person
+people = [
+  {
+      name: 'Sarah',
+      transactions: [
+          {
+              type: 'COFFEE',
+              amount: 7.43
+          },
+          {
+              type: 'TACOS',
+              amount: 14.65
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: 'Bandari',
+      transactions: [
+          {
+              type: 'BIKES',
+              amount: 800.00
+          },
+          {
+              type: 'TACOS',
+              amount: 14.65
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: 'Safwan',
+      transactions: [
+          {
+              type: 'COFFEE',
+              amount: 7.43
+          },
+          {
+              type: 'COFFEE',
+              amount: 100.00
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  }
+]
+
+
+coffee_average_per_person = []
+
+for person in people
+    coffee_counter=0
+    personal_sum=0
+    for transaction in person[:transactions]
+        if transaction[:type]=='COFFEE'
+            coffee_counter+=1
+            personal_sum+=transaction[:amount]
+        end
+    end
+    coffee_average_per_person.push({name: person[:name],average: (personal_sum/coffee_counter)})
+end
+p coffee_average_per_person
