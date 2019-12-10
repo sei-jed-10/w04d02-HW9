f44bb413100bdd30d8a612cccec71ee75c50042b
Mohammedalamry <Mohammedalamry@users.noreply.github.com>
2019-12-10 08:31:51 +0300

    Hw9notcomplete

---
+++ HW.9.rb
@@ -0,0 +1,20 @@
+students = [
+  {
+      first_name: 'Abdulrahman',
+      last_name: 'Alsulami'
+  },
+  {
+      first_name: 'Leena',
+      last_name: 'Yaseen',
+  },
+  {
+      first_name: 'Sara',
+      last_name: 'Alraddadi',
+  }
+]
+
+upper_case_full_names = []
+
+students.map do |key, val|   
+
+
